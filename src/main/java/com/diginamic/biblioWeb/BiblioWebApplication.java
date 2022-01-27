cfbec19ca76c73a285f5f4aac981bfbdc0ac0948
Boris Ahumada <boris.ahumada@laposte.net>
2022-01-27 11:27:10 +0100

    init

---
+++ src/main/java/com/diginamic/biblioWeb/BiblioWebApplication.java
@@ -0,0 +1,13 @@
+package com.diginamic.biblioWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioWebApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/diginamic/biblioWeb/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.diginamic.biblioWeb;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BiblioWebApplication.class);
+	}
+
+}
